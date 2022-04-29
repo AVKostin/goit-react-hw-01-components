db960bf6c2552d7e2256b0dd3780e51597c06c22
AVKostin <alexander.kostin.ua@gmail.com>
2022-04-29 20:00:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 // import React from 'react';
-import ReactDOM from 'react-dom';
-
-
+// import ReactDOM from 'react-dom';
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
