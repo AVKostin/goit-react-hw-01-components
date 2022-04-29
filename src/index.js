0dd0c3537b638a6a8f408d2a779534656e0ec23e
AVKostin <alexander.kostin.ua@gmail.com>
2022-04-29 19:38:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,7 @@
 // import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const elem1 = React.createElement('span', { children: 'Привет' });
-// const elem2 = React.createElement('span', { children: 'мир' });
 
-const elem1 = <span>Привет</span>;
-const elem2 = <span>мир</span>;
-
-// const element = React.createElement('div', {
-//   a: 5,
-//   b: 10,
-//   children: ['Привет мир'],
-// });
-const jsxElements = (
-  <div>
-    {elem1}
-    {elem2}
-  </div>
-);
-
-console.log(jsxElements);
-
-// ReactDOM.render(element, document.querySelector('#root'));
-
-// ===== версия кодя под React 18? но ещё не точно. ======
-// import React from 'react';
-// import createRoot from 'react-dom/client';
-
-// const element = React.createElement('div', {
-//   a: 5,
-//   b: 10,
-//   children: 'Привет мир',
-// });
-
-// console.log(element);
-
-// const root = createRoot(element);
-
-// console.log(root);
-// ========================================================
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
