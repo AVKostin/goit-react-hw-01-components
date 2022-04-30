9f5861b2b8ad595c7d38dd994305ca9e7f823a70
AVKostin <alexander.kostin.ua@gmail.com>
2022-04-30 17:15:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import Container from './components/Container/Container';
-import Profile from './components/social-profile/Profile';
+import user from './data/user.json';
+import Profile from './components/Profile/Profile';
 
 export default function App() {
   return (
-    <Container>
-      <Profile type="small" classNames="big red" styles={{ color: '#fff' }} />
-      {/* <Profile type="medium" />
-      <Profile type="large" /> */}
-    </Container>
+    <div>
+      <Profile
+        userName={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
   );
 }
+
+//  <Statistics title="Upload stats" data={data} />
+//     <FriendList friends={friends} />
+//     <Statistics data={data} />
+//     <TransactionHistory items={transactions} />
