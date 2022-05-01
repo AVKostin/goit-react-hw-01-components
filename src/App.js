8c33ee6caea9c6fecd0ec8e04c394161e1811d97
AVKostin <alexander.kostin.ua@gmail.com>
2022-05-01 12:20:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import user from './data/user.json';
 import Profile from './components/Profile/Profile';
+import Statistics from 'components/Statistics/Statistics';
 import s from './components/Container/Container.module.css';
 
-export default function App() {
+export default function App({ data }) {
   return (
     <div className={s.container}>
       <Profile
@@ -12,6 +13,7 @@ export default function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" data={data} />
     </div>
   );
 }
