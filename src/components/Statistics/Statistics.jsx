fc8b62d9b4143b3b74dc9b4f2dec7888856548fa
AVKostin <alexander.kostin.ua@gmail.com>
2022-05-01 12:20:33 +0300

    Update Statistics.jsx

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+
+function Statistics({ title, data }) {
+    return (
+    <section className={s.statistics}>
+        {title && <h2 className={s.title}>{title}</h2>}
+
+            <ul className={s.list}>
+                {data.map(item => (
+                <li
+                    className={s.item}
+                    key={item.id}
+                    style={{ backgroundColor: randomColor() }}
+                >
+                    <span className={s.label}>{item.label}</span>
+                    <span className={s.percentage}>{item.percentage}%</span>
+                </li>
+                ))}
+            </ul>
+    </section>
+    );
+};
+
+export default Statistics;
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    data: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+      })
+    ),
+  };
