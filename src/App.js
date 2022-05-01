e15965a31625bae15462b05841caffd7c93587b3
AVKostin <alexander.kostin.ua@gmail.com>
2022-05-01 19:32:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import user from './components/data/user.json';
 import Profile from './components/Profile/Profile';
 import Statistics from 'components/Statistics/Statistics';
+import Transactions from './components/Transactions/Transactions';
 import Friends from 'components/Friends/Friends';
 import data from './components/data/data.json';
 import friends from 'components/data/friends.json';
+import transactions from './components/data/transactions.json';
 import s from './components/Container/Container.module.css';
 
 export default function App() {
@@ -18,10 +20,7 @@ export default function App() {
       />
       <Statistics title="Upload stats" data={data} />
       <Friends friends={friends} />
+      <Transactions items={transactions} />
     </div>
   );
 }
-
-//
-//     <Statistics data={data} />
-//     <TransactionHistory items={transactions} />
